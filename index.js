9454ab48a155aa85b7b64d7d04b0ac6a4700e911
Muhammad Osaid Siddiqui <123214548+osaid4624@users.noreply.github.com>
2024-05-30 04:36:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+//Assiment No1 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// const name = "Hello Eric";
+// console.log(`${name}  "would you like to learn some Python today?"`);
+//Assiment complete
+Object.defineProperty(exports, "__esModule", { value: true });
+//Assiment No2 Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// const name = "Eric"
+// console.log(name.toLocaleUpperCase());
+// console.log(name.toLocaleLowerCase());
+// console.log(name.charAt(0).toLocaleUpperCase()+name.slice(1).toLowerCase());
+//Complete
+//Assiment No3 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// let name = "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
+// console.log(name);
+// //Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// let famous_person = "Albert Einstein";
+// console.log(`${famous_person}  once said, “A person who never made a mistake never tried anything new.”`);
+//Assment No 4 
+// const name: string = "\t\n Asharib \t\n"; // This saves the name with whitespace
+// console.log( name);
+// console.log(name.trim());
+//complete 
+//Assiment No 5
+//  console.log(5+3);
+//  console.log(2*4);
+//  console.log(10-2);
+//  console.log(16/2);
+// complete 
+//Assiment No6 
+//  let num:number = 5;
+//  let num1:number = 3;
+//  console.log(`my favroit number is ${num}`);
+//Assoment compelte 
+//Assiment No 7 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Name Usaid // 23/5/24
+// let name = "usaid " 
+// console.log(`Hello ${name}I'm very enjoy to learn typescipt`);
+// Complete 
+// Assiment No 8 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//  let name:string[]=['usaid','ali','hamza','hussain']
+//     for(let i=0; i<name.length; i++)
+//     console.log(name[i]); // Jo specific name show karwna ha uska index number likho
+// Complete 
+//Assimemt No 9 
+let names = ['usaid', 'ali', 'hamza', 'hussain'];
+for (let name of names) {
+    console.log(`${name} "would you like to learn typescript`);
+}
