8d9ad497324f35c20ff16e4f639b17ac7795a757
Muhammad Osaid Siddiqui <123214548+osaid4624@users.noreply.github.com>
2024-06-05 03:18:07 +0500

    update

--- index.ts
+++ index.ts
@@ -71,3 +71,114 @@
 // }
 
 //Assiment complete
+
+//Assiment No 10 Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+// Use a for each loop
+// let car :string[]=['audi','honda','toyota','civic']
+
+// car.forEach(car=>{
+//     console.log(`I would like to own a ${car}`);
+    
+// })
+
+//Complete
+
+//Assiment No11 
+//USe a for each loop
+//  let gussetName :string[]=['Usaid','Ali','Hamza','Huzaifia']
+
+//  gussetName.unshift("Nikola Tesla")
+//  gussetName.splice(2,0,"John")
+//  gussetName.push("david")
+//  gussetName.forEach(gussetName=>{
+//      console.log(`Dear ${gussetName}, would you like to join me for dinner?`);
+
+
+
+
+//  })
+
+ 
+//Complete
+
+// Assiment 11    //Discord assiment 15
+
+// let unabletoAttande="Usaid"
+// console.log(`${unabletoAttande} can ,make it a dinner`);
+// // Replace the guesst 
+// let newGuesst="hamza";
+// gussetName[gussetName.indexOf(unabletoAttande)]= newGuesst 
+// // New Invatition
+// gussetName.forEach(gussetName=>{
+//     console.log(`Dear ${gussetName} would you like to join my partry`);
+    
+// })
+
+// //complete
+
+//Assiment 12
+//  let name:string []=['usaid','asad','hamza','John']
+//  console.log("Great News For add New Guesst !");
+
+//  // Adding more gusset  name.unshift("Issac Newton")
+//  name.splice(2,0, "Hussain")
+//  name.push("Baber")
+
+//  name.forEach(name=>{
+//      console.log(`Dear ${name} Would You like to Join Dinner with me ?`);
+    
+//  })
+//  //complete 
+
+// // Assiment 13
+
+// console.log("Ohh sorry unfortunately I'm invite only two friends Sorry");
+
+// // While loop
+// while(name.length<2){
+//     let removeGuests = name.pop()
+//     console.log(`Sorry ${removeGuests} I can't invite you `);
+
+// // for each loop 
+// name.forEach(name=>{
+//     console.log(`Dear ${name} Invite only you So I'm waiting for Today Dinner !`)
+// })}
+
+// name.splice(2,name.length)
+// console.log(name);
+
+
+// Assiment 14 
+// let listName:string[]=['hello world','Today is weather is very beatuiful','I going to cenima ','I start a learn typscript',]
+// console.log("Orignal Order", listName);
+//  console.log("Alpebatic Order", [...listName].sort());
+//  console.log("Orginal Order", listName);
+//  console.log("Revers Alphabet",[...listName].sort().reverse());
+//  console.log("Orignal order ", listName);
+
+// listName.reverse();
+// console.log("Reverse order",listName);
+// listName.reverse()
+// console.log("orginal order", listName);
+// listName.sort()
+// console.log("Sort order Name ", listName);
+// listName.reverse()
+// console.log("revers alphabatic ", listName);
+
+// complete
+
+//Assiment 15 
+// let gueestName:string[]=['Usaid',"hamza","Obaid","Ali"]
+// console.log(` I'm inviting  ${gueestName.length} people to fiends `);
+ 
+// complete 
+
+// Asssiment 16
+
+let listName:string[]=['United Kindom','canada','USA']
+console.log("My favriot Country ", listName);
+
+//complete 
+
+
+ 
